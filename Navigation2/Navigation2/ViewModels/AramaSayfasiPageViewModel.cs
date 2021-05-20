7a34c68eb8aa016ea7c43c37652edd82efce77b1
Fatih Özgür <fatihozgurrgal@gmail.com>
2021-05-20 22:50:04 +0300

    Arama Sayfası eklendi

---
+++ Navigation2/Navigation2/ViewModels/AramaSayfasiPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Navigation2.Services;
+using Navigation2.Models;
+
+namespace Navigation2.ViewModels
+{
+    class AramaSayfasiPageViewModel : BaseViewModel
+    {
+        public string Keyword { get; set; }
+        public Command SearchCommand { get; }
+
+        public AramaSayfasiPageViewModel()
+        {
+            SearchCommand = new Command(SearchByName);
+        }
+
+        private void SearchByName()
+        {
+            DataHolder.GetProductsByName("PRO");
+            Products = DataHolder.FilteredList;
+        }
+
+    }
+}
---
+++ Navigation2/Navigation2/Views/AramaSayfasi.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Navigation2.Models;
+using Navigation2.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Navigation2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AramaSayfasi : ContentPage
+    {
+        public AramaSayfasi()
+        {
+            InitializeComponent();
+            BindingContext = new AramaSayfasiPageViewModel();
+        }
+        async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedProduct = ((CollectionView)sender).SelectedItem as Product;
+            if (selectedProduct == null)
+                return;
+
+            await Navigation.PushAsync(new UrunDetay(selectedProduct));
+
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
+}
